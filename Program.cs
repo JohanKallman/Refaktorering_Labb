5374356691db117d1c5e3589eaa7a64fa461d81c
JohanKallman <johan.kallman@iths.se>
2022-11-22 14:42:08 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Refaktorering_Labb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
