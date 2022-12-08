150ae173ca9de6773ec2634ff46287121ae6a534
JohanKallman <johan.kallman@iths.se>
2022-12-08 21:11:54 +0100

    added unit test and created 2 test methods with actual methods that pass the tests

--- Models/Mastermind.cs
+++ Models/Mastermind.cs
@@ -1,50 +1,59 @@
+using System;
 using System.Linq;
 
 namespace Refactoring_Lab.Models
 {
     public class Mastermind : Game
-  {
-
-
-    public Mastermind()
-    {
-      GameName = "Mastermind";
-      Rules = "Guess must contain 4 different colors. Choose between (1)Red, (2)Green, (3)Blue, (4)Yellow, (5)Cyan or (6)Magenta.";
-
-      GameIsRunning = false;
-      PlayerGuessing = true;
-      CorrectAnswer = "";
-      NumberOfGuesses = 1;
-      HighestRandomNumber = 7;
-      AmountOfIntegersInAnswer = 4;
-
-    }
-
-
-    public override void PrepareRoundResult()
     {
 
-      OutPutResult = ReturnOutputAfterGuess();
-      FromNumberToColor();
 
+        public Mastermind()
+        {
+            GameName = "Mastermind";
+            Rules = "Guess must contain 4 different colors. Choose between (1)Red, (2)Green, (3)Blue, (4)Yellow, (5)Cyan or (6)Magenta.";
+
+            GameIsRunning = false;
+            PlayerGuessing = true;
+            CorrectAnswer = "";
+            NumberOfGuesses = 1;
+            HighestRandomNumber = 7;
+            AmountOfIntegersInAnswer = 4;
+            LowestRandomNumber = 1;
+        }
+
+        public override void PrepareRoundResult()
+        {
+            OutPutResult = ReturnOutputAfterGuess();
+
+            PlayerGuess = FromNumberToColor();
+        }
+
+        //public void FromNumberToColor()
+        //{
+        //    int[] intArray = ConvertStringToIntArray();
+        //    PlayerGuess = "";
+        //    foreach (var number in intArray)
+        //    {
+        //        PlayerGuess += (Color)number + ",";
+        //    }
+        //}
+
+        public string FromNumberToColor()
+        {
+            var intArray = ConvertStringToIntArray();
+            PlayerGuess = "";
+            foreach (var number in intArray)
+            {
+                PlayerGuess += (Color)number + ", ";
+            }
+            return PlayerGuess;
+        }
+
+
+        public int[] ConvertStringToIntArray()
+        {
+            //return PlayerGuess.Select(c => c - '0').ToArray();
+            return PlayerGuess.Select(x => int.Parse(x.ToString())).ToArray();
+        }
     }
-    public void FromNumberToColor()
-    {
-
-      //Skapa test och metod och bryt ut konverteringen
-      var intArray = PlayerGuess.Select(c => c - '0').ToArray();
-      PlayerGuess = "";
-      foreach (var number in intArray)
-      {
-
-        PlayerGuess += (Color)number + ",";
-      }
-
-
-
-    }
-
-
-
-  }
 }
--- Models/MooGame.cs
+++ Models/MooGame.cs
@@ -1,29 +1,30 @@
 namespace Refactoring_Lab.Models
 {
     public class MooGame : Game
-  {
-
-    public MooGame()
-    {
-      GameName = "Moo Game";
-      Rules = "Guess must contain 4 characters between 0-9!";
-      TopOfGuessingNumberSpan = 10;
-      GameIsRunning = false;
-      PlayerGuessing = true;
-      CorrectAnswer = "";
-      NumberOfGuesses = 0;
-      HighestRandomNumber = 10;
-      AmountOfIntegersInAnswer = 4;
-
-    }
-
-
-    public override void PrepareRoundResult()
     {
 
-      OutPutResult = ReturnOutputAfterGuess();
+        public MooGame()
+        {
+            GameName = "Moo Game";
+            Rules = "Guess must contain 4 characters between 0-9!";
+            TopOfGuessingNumberSpan = 10;
+            GameIsRunning = false;
+            PlayerGuessing = true;
+            CorrectAnswer = "";
+            NumberOfGuesses = 0;
+            HighestRandomNumber = 10;
+            AmountOfIntegersInAnswer = 4;
+            LowestRandomNumber = 0;
+
+        }
+
+
+        public override void PrepareRoundResult()
+        {
+
+            OutPutResult = ReturnOutputAfterGuess();
+
+        }
 
     }
-
-  }
 }
