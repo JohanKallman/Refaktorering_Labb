914ea5892dd2e4c11f1f945adf0c1cbed5760e3d
Petter <petter.berelin@gmail.com>
2022-12-14 14:41:44 +0100

    Fixed result output for Mastermind

--- Models/Game.cs
+++ Models/Game.cs
@@ -7,235 +7,148 @@ namespace Refactoring_Lab.Models
   {
     public string GameName { get; set; }
     public string Rules { get; set; }
-    public bool GameIsRunning { get; set; }
-    public abstract class Game
+    public bool GameIsRunning { get; set; } = false;
+    public GameAnswer GameAnswer { get; set; } = new GameAnswer();
+    public PlayerGuess PlayerGuess { get; set; } = new PlayerGuess();
+
+
+    public void StartNewInstanceOfGame()
     {
-      public string GameName { get; set; }
-      public string Rules { get; set; }
-      public bool GameIsRunning { get; set; } = false;
-      public GameAnswer GameAnswer { get; set; } = new GameAnswer();
+      ResetGuessingCounter();
+      GameAnswer.CorrectAnswer = GenerateCorrectAnswer();
+      Console.WriteLine("For practice, number is: " + GameAnswer.CorrectAnswer + "\n");
+    }
 
-      // Ny klass Answer?
-      public string CorrectAnswer { get; set; }
-      public int HighestRandomNumber { get; set; }
-      public int LowestRandomNumber { get; set; }
-      public int AmountOfIntegersInAnswer { get; set; }
 
-      // Ny klass PlayerGuess?
-      public string OutPutResult { get; set; }
-      public bool PlayerIsGuessing { get; set; }
-      public string PlayerGuess { get; set; }
-      public int NumberOfGuesses { get; set; }
-      public bool ValidGuess { get; set; }
-      public PlayerGuess PlayerGuess { get; set; } = new PlayerGuess();
+    public virtual void PrepareRoundResult()
+    {
+      PlayerGuess.OutPutResult = ReturnOutputAfterGuess();
+    }
 
-      public void StartNewInstanceOfGame()
+    public string GenerateCorrectAnswer()
+    {
+      Random numberGenerator = new Random();
+      string correctAnswer = "";
+      while (correctAnswer.Length < GameAnswer.AmountOfIntegersInAnswer)
       {
-        ResetGuessingCounter();
-        CorrectAnswer = GenerateCorrectAnswer();
+        int newNumber = numberGenerator.Next(GameAnswer.LowestRandomNumber, GameAnswer.HighestRandomNumber);
+        correctAnswer = CheckIfUniqueNumberIsRequired(correctAnswer, newNumber);
+      }
+      return "4423";
+      //return correctAnswer;
+    }
 
-        Console.WriteLine("For practice, number is: " + CorrectAnswer + "\n"); // Till UI
+    public virtual string CheckIfUniqueNumberIsRequired(string correctAnswer, int newNumber)
+    {
+      if (!correctAnswer.Contains(newNumber.ToString()))
+      {
+        correctAnswer += newNumber.ToString();
       }
 
-      public void StartNewInstanceOfGame()
+      return correctAnswer;
+    }
+
+
+    public void ResetGuessingCounter()
+    {
+      PlayerGuess.NumberOfGuesses = 0;
+    }
+
+
+    public void PlayerGuesses()
+    {
+      PlayerGuess.Guess = Console.ReadLine().Trim();
+      IncrementGuessingCounterByOne();
+    }
+
+
+
+
+    public void IncrementGuessingCounterByOne()
+    {
+      PlayerGuess.NumberOfGuesses++;
+    }
+
+
+    public void ValidateInputGuess()
+    {
+      PlayerGuess.IsValidGuess = CheckIfAcceptedFormat();
+    }
+
+
+    public bool CheckIfAcceptedFormat()
+    {
+      bool guessHasCorrectFormat = CheckIfCorrectLengthFormat();
+      if (guessHasCorrectFormat == false)
       {
-        ResetGuessingCounter();
-        GameAnswer.CorrectAnswer = GenerateCorrectAnswer();
-        Console.WriteLine("For practice, number is: " + GameAnswer.CorrectAnswer + "\n");
-      }
-
-      public virtual void PrepareRoundResult()
-      {
-        OutPutResult = ReturnOutputAfterGuess();
-      }
-      public virtual void PrepareRoundResult()
-      {
-        PlayerGuess.OutPutResult = ReturnOutputAfterGuess();
-      }
-
-      public string GenerateCorrectAnswer()
-      {
-        Random numberGenerator = new Random();
-        string uniqueNumberToReturn = "";
-        while (uniqueNumberToReturn.Length < AmountOfIntegersInAnswer)
-        {
-          int newNumber = numberGenerator.Next(LowestRandomNumber, HighestRandomNumber);
-          if (!uniqueNumberToReturn.Contains(newNumber.ToString()))
-          {
-            uniqueNumberToReturn += newNumber.ToString();
-          }
-        }
-        return uniqueNumberToReturn;
-      }
-
-      public void ResetGuessingCounter()
-      {
-        NumberOfGuesses = 0;
-      }
-      public void ResetGuessingCounter()
-      {
-        PlayerGuess.NumberOfGuesses = 0;
-      }
-
-      public void PlayerGuesses()
-      {
-        PlayerGuess = Console.ReadLine().Trim();
-        IncrementGuessingCounterByOne();
-      }
-      public void PlayerGuesses()
-      {
-        PlayerGuess.Guess = Console.ReadLine().Trim();
-        IncrementGuessingCounterByOne();
-      }
-
-      public void IncrementGuessingCounterByOne()
-      {
-        NumberOfGuesses++;
-
-      }
-
-
-      public void IncrementGuessingCounterByOne()
-      {
-        PlayerGuess.NumberOfGuesses++;
-      }
-
-      public void ValidateInputGuess()
-      {
-        ValidGuess = CheckIfAcceptedFormat();
-
-        // bool guessHasCorrectFormat = CheckIfCorrectLengthFormat();
-
-        // if (guessHasCorrectFormat == false)
-        // {
-        //   return false;
-        // }
-
-        // return guessHasCorrectFormat = CheckIfCorrectCharFormat();
-
-      }
-      public void ValidateInputGuess()
-      {
-        PlayerGuess.IsValidGuess = CheckIfAcceptedFormat();
-      }
-
-      public bool CheckIfAcceptedFormat()
-      {
-        bool guessHasCorrectFormat = CheckIfCorrectLengthFormat();
-
-        if (guessHasCorrectFormat == false)
-        {
-          return false;
-        }
-
-        return guessHasCorrectFormat = CheckIfCorrectCharFormat();
-      }
-      public bool CheckIfAcceptedFormat()
-      {
-        bool guessHasCorrectFormat = CheckIfCorrectLengthFormat();
-        if (guessHasCorrectFormat == false)
-        {
-          return false;
-        }
-        return guessHasCorrectFormat = CheckIfCorrectCharFormat();
-      }
-
-      public bool CheckIfCorrectLengthFormat()
-      {
-
-        if (PlayerGuess.Length == AmountOfIntegersInAnswer)
-        {
-          return true;
-        }
-
         return false;
       }
+      return guessHasCorrectFormat = CheckIfCorrectCharFormat();
+    }
 
-      // public bool CheckIfCorrectLengthFormat()
-      // {
-      //   int numberOfAllowedCharachters = 4;
-      //   if (PlayerGuess.Length == numberOfAllowedCharachters)
-      //   {
-      //     return true;
-      //   }
 
-      //   return false;
-      // }
-      public bool CheckIfCorrectLengthFormat()
+
+
+    public bool CheckIfCorrectLengthFormat()
+    {
+      if (PlayerGuess.Guess.Length == GameAnswer.AmountOfIntegersInAnswer)
       {
-        if (PlayerGuess.Guess.Length == GameAnswer.AmountOfIntegersInAnswer)
-        {
-          return true;
-        }
-        return false;
+        return true;
       }
+      return false;
+    }
 
-      public bool CheckIfCorrectCharFormat()
+    public bool CheckIfCorrectCharFormat()
+    {
+      if (PlayerGuess.Guess.All(char.IsDigit))
       {
-        if (PlayerGuess.All(char.IsDigit))
-        {
-          return true;
-        }
-
-        return false;
-      }
-
-      // public string PrepareRoundResult()
-      // {
-
-      //   return ReturnOutputAfterGuess();
-      // }
-
-      public string ReturnOutputAfterGuess()
-      {
-        int numberExistsWrongPositionCounter = 0;
-        int correctPositionCounter = 0;
-
-        for (int i = 0; i < 4; i++)
-        {
-          for (int j = 0; j < 4; j++)
-          {
-            if (CorrectAnswer[i] == PlayerGuess[j])
-            {
-              if (i == j)
-              {
-                correctPositionCounter++;
-              }
-              else
-              {
-                numberExistsWrongPositionCounter++;
-              }
-            }
-          }
-        }
-        return "BBBB".Substring(0, correctPositionCounter) + "," + "CCCC".Substring(0, numberExistsWrongPositionCounter);
-      }
-
-      public void CheckIfGameIsOver()
-      {
-        if (OutPutResult == "BBBB,")
-        {
-          PlayerIsGuessing = false;
-        }
-
-
-      }
-      public bool CheckIfPlayAgain()
-      {
-        if (Console.ReadLine().ToUpper() == "Q")
-        {
-          return false;
-        }
         return true;
       }
 
+      return false;
     }
+
+
+
+    public string ReturnOutputAfterGuess()
+    {
+      int numberExistsWrongPositionCounter = 0;
+      int correctPositionCounter = 0;
+      string[] correctAnswerCheck = { "", "", "", "" };
+      string[] correctGuessCheck = { "", "", "", "" };
+      for (int i = 0; i < 4; i++)
+      {
+        for (int j = 0; j < 4; j++)
+        {
+          if (GameAnswer.CorrectAnswer[i] == PlayerGuess.Guess[j] && correctAnswerCheck[i] != "X" && correctGuessCheck[j] != "X")
+          {
+            correctAnswerCheck[i] = "X";
+            correctGuessCheck[j] = "X";
+
+            if (i == j)
+            {
+              correctPositionCounter++;
+
+            }
+            else
+            {
+              numberExistsWrongPositionCounter++;
+            }
+
+          }
+        }
+      }
+      return "BBBB".Substring(0, correctPositionCounter) + "," + "CCCC".Substring(0, numberExistsWrongPositionCounter);
+    }
+
     public void CheckIfGameIsOver()
     {
       if (PlayerGuess.OutPutResult == "BBBB,")
       {
         PlayerGuess.PlayerIsGuessing = false;
       }
+
+
     }
     public bool CheckIfPlayAgain()
     {
@@ -245,7 +158,9 @@ namespace Refactoring_Lab.Models
       }
       return true;
     }
+
   }
+
 
 
 }
--- Models/Mastermind.cs
+++ Models/Mastermind.cs
@@ -5,83 +5,48 @@ namespace Refactoring_Lab.Models
 {
   public class Mastermind : Game
   {
-
-
     public Mastermind()
     {
       GameName = "Mastermind";
       Rules = "\nGuess must contain 4 different colors. \nChoose between (1)Red, (2)Green, (3)Blue, (4)Yellow, (5)Cyan or (6)Magenta.";
-
-      GameIsRunning = false;
-      CorrectAnswer = "";
-      NumberOfGuesses = 1;
-      HighestRandomNumber = 7;
-      AmountOfIntegersInAnswer = 4;
-      LowestRandomNumber = 1;
+      PlayerGuess.NumberOfGuesses = 1;
+      GameAnswer.HighestRandomNumber = 7;
+      GameAnswer.AmountOfIntegersInAnswer = 4;
+      GameAnswer.LowestRandomNumber = 1;
     }
-    public class Mastermind : Game
+
+    public override void PrepareRoundResult()
     {
-      public Mastermind()
-      {
-        GameName = "Mastermind";
-        Rules = "\nGuess must contain 4 different colors. \nChoose between (1)Red, (2)Green, (3)Blue, (4)Yellow, (5)Cyan or (6)Magenta.";
-        PlayerGuess.NumberOfGuesses = 1;
-        GameAnswer.HighestRandomNumber = 7;
-        GameAnswer.AmountOfIntegersInAnswer = 4;
-        GameAnswer.LowestRandomNumber = 1;
-      }
+      PlayerGuess.OutPutResult = ReturnOutputAfterGuess();
 
-      public override void PrepareRoundResult()
-      {
-        OutPutResult = ReturnOutputAfterGuess();
-
-        PlayerGuess = FromNumberToColor();
-      }
-
-      //public void FromNumberToColor()
-      //{
-      //    int[] intArray = ConvertStringToIntArray();
-      //    PlayerGuess = "";
-      //    foreach (var number in intArray)
-      //    {
-      //        PlayerGuess += (Color)number + ",";
-      //    }
-      //}
-
-      public string FromNumberToColor()
-      {
-        var intArray = ConvertStringToIntArray();
-        PlayerGuess = "";
-        foreach (var number in intArray)
-        {
-          PlayerGuess += (Color)number + ", ";
-        }
-
-        return PlayerGuess;
-
-      }
-
-      public string FromNumberToColor()
-      {
-        var intArray = ConvertStringToIntArray();
-        PlayerGuess.Guess = "";
-        foreach (var number in intArray)
-        {
-          PlayerGuess.Guess += (Color)number + ", ";
-        }
-        return PlayerGuess.Guess;
-      }
-
-      public int[] ConvertStringToIntArray()
-      {
-        //return PlayerGuess.Select(c => c - '0').ToArray();
-        return PlayerGuess.Select(x => int.Parse(x.ToString())).ToArray();
-      }
+      PlayerGuess.Guess = FromNumberToColor();
     }
+
+    public override string CheckIfUniqueNumberIsRequired(string correctAnswer, int newNumber)
+    {
+      return correctAnswer += newNumber.ToString();
+    }
+
+
+    public string FromNumberToColor()
+    {
+      var intArray = ConvertStringToIntArray();
+      PlayerGuess.Guess = "";
+      foreach (var number in intArray)
+      {
+        PlayerGuess.Guess += (Color)number + ", ";
+      }
+
+      return PlayerGuess.Guess;
+
+    }
+
     public int[] ConvertStringToIntArray()
     {
       //return PlayerGuess.Select(c => c - '0').ToArray();
       return PlayerGuess.Guess.Select(x => int.Parse(x.ToString())).ToArray();
     }
+
+
   }
 }
